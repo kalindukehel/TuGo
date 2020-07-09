43e211d769e386e4426c0b10427b367e9f415e2b
kush_p7 <kush@hyperpad.com>
2020-07-09 00:07:18 -0400

    check api requests

--- TuGo/App.js
+++ TuGo/App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from "expo-status-bar";
+//To Make HTTP API call in React Native using Axios https://aboutreact.com
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+//import React in our code.
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+//import all the components we are going to use.
+import axios from "axios";
 
-export default function App() {
-  console.log("Hello World from App.js");
+const App = () => {
+  const getDataUsingAsyncAwaitGetCall = async () => {
+    try {
+      const response = axios.get(`http://127.0.0.1:8000/api/accounts/`);
+      alert(JSON.stringify(response.data));
+    } catch (error) {
+      // handle error
+      console.log(error);
+      alert(error.message);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>TuGo</Text>
+    <View style={styles.MainContainer}>
+      <Text style={{ fontSize: 30, textAlign: "center" }}>
+        Example of Axios Networking in React Native
+      </Text>
+      {/*Running GET Request*/}
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={getDataUsingAsyncAwaitGetCall}
+      >
+        <Text>Get Data Using Async Await GET</Text>
+      </TouchableOpacity>
+
+      <Text style={{ textAlign: "center", marginTop: 18 }}>
+        www.aboutreact.com
+      </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+  MainContainer: {
     justifyContent: "center",
+    flex: 1,
+    padding: 16,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    width: "100%",
+    marginTop: 16,
   },
 });
+
+export default App;
