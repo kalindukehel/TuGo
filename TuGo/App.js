920a0d875d1ea76f952f9f9f8c4442530a38321d
Kalindu <59674165+kalindu-k@users.noreply.github.com>
2020-07-10 16:40:59 -0400

    Add react native get from API calls and add gitignore

--- TuGo/App.js
+++ TuGo/App.js
@@ -4,39 +4,41 @@ import React from "react";
 import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 //import all the components we are going to use.
 import axios from "axios";
-
-const App = () => {
-  const getDataUsingAsyncAwaitGetCall = async () => {
-    try {
-      const response = axios.get(`http://127.0.0.1:8000/api/accounts/`);
-      alert(JSON.stringify(response.data));
-    } catch (error) {
-      // handle error
-      console.log(error);
-      alert(error.message);
+import {API_URL} from "./constants"
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      info:null
     }
-  };
+  }
 
-  return (
+  componentDidMount(){
+    fetch(`${API_URL}/api/accounts/1`)
+      .then(response=>response.json())
+      .then(resp=>{
+        console.log(resp)
+      })
+
+
+  }
+
+  render(){
+    return (
     <View style={styles.MainContainer}>
       <Text style={{ fontSize: 30, textAlign: "center" }}>
         Example of Axios Networking in React Native
       </Text>
       {/*Running GET Request*/}
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={getDataUsingAsyncAwaitGetCall}
-      >
-        <Text>Get Data Using Async Await GET</Text>
-      </TouchableOpacity>
+      <Text>{this.state.info}</Text>
 
       <Text style={{ textAlign: "center", marginTop: 18 }}>
         www.aboutreact.com
       </Text>
     </View>
   );
-};
+};}
 
 const styles = StyleSheet.create({
   MainContainer: {
