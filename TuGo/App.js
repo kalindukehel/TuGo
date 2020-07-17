7647f627bf3e45c9f8bf989dd27daf02b4399a06
Kalindu <59674165+kalindu-k@users.noreply.github.com>
2020-07-17 13:31:10 -0400

    Add search and create user

--- TuGo/App.js
+++ TuGo/App.js
@@ -1,11 +1,14 @@
 //To Make HTTP API call in React Native using Axios https://aboutreact.com
-import React from "react";
+ import React, { useState, useEffect } from "react";
 //import React in our code.
-import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Text, Button, TextInput } from "react-native";
 //import all the components we are going to use.
 import axios from "axios";
 import { API_URL } from "./constants";
 import useAccounts from "./app.hooks";
+import { render } from "react-dom";
+import { getAccountById as getAccountByIdAPI, createAccount as createAccountAPI } from "./src/api";
+
 const App = () => {
   // componentDidMount(){
   //   fetch(`${API_URL}/api/accounts/1`)
@@ -19,18 +22,41 @@ const App = () => {
   //     .catch(error=>{
   //       console.log(error)
   //     })
- const accounts = useAccounts().map(account=>{
-   return <Text>{"\n"}Name: {account.name} || Username: {account.username}</Text>
- });
- 
+
+  const [accounts,setAccounts] = useState()
+  async function handleChange(text){
+    try{
+      const res = await getAccountByIdAPI(text);
+      setAccounts(res.data)
+      console.log(res.data)
+    }catch(error){
+      console.log("error")
+    }
+  }
+
+  async function createAccount(){
+    try{
+      const data = {
+        username: "test",
+        email: "test@gmail.com",
+        name: "tester"
+      }
+      createAccountAPI(data)
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+
   return (
     <View style={styles.MainContainer}>
       <Text style={{ fontSize: 30, textAlign: "center" }}>
         Example of Axios Networking in React Native
       </Text>
+      <Text>{accounts?accounts.username:null}</Text>
+      <Button title="click me" onPress={createAccount} ></Button>
+      <TextInput onChangeText={handleChange} style={{"borderColor":"black","borderWidth":"1px"}}>asd</TextInput>
       {/*Running GET Request*/}
-
-      {accounts != undefined? <Text>{accounts}</Text>:null}
       <Text style={{ textAlign: "center", marginTop: 18 }}></Text>
     </View>
   );
--- TuGo/src/api.js
+++ TuGo/src/api.js
@@ -4,3 +4,15 @@ import { API_URL } from "../constants";
 export async function getAccounts() {
   return axios.get(`${API_URL}/api/accounts`);
 }
+
+export async function getAccountById(id){
+  return axios.get(`${API_URL}/api/accounts/${id}`);
+}
+
+export async function createAccount(data){
+  return axios.post(`${API_URL}/api/accounts/`,{
+    username: data.username,
+    email: data.email,
+    name: data.name
+  })
+}
