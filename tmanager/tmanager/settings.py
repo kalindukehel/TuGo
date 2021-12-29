6085dfed2fc515d772a10bd493b4df1288f82bda
Kalindu Kehel Baddarage <59674165+kalindukehel@users.noreply.github.com>
2021-12-28 22:43:54 -0500

    Change local s3 default ACL. Change youtube video url migration

--- tmanager/tmanager/settings.py
+++ tmanager/tmanager/settings.py
@@ -57,7 +57,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'static'
 AWS_LOCATION_MEDIA = 'media'
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
